4dbc4576639b76059b04871dbff94e11b16e05b3
sizer <shahin.reza@brainstation-23.com>
2024-06-07 12:29:53 +0600

    refactor: login feature completed

--- lib/feature/login/domain/repositories/login_repository.g.dart
+++ lib/feature/login/domain/repositories/login_repository.g.dart
@@ -1,28 +1,26 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'remote_data_source.dart';
+part of 'login_repository.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$loginRemoteDataSourceHash() =>
-    r'8678981efafa8cb1a9143ffac18a8f026332cf41';
+String _$loginRepositoryHash() => r'4f7e7f6e8c94e08a523477e7593851077cb98bd6';
 
-/// See also [loginRemoteDataSource].
-@ProviderFor(loginRemoteDataSource)
-final loginRemoteDataSourceProvider =
-    AutoDisposeProvider<LoginRemoteDataSource>.internal(
-  loginRemoteDataSource,
-  name: r'loginRemoteDataSourceProvider',
+/// See also [loginRepository].
+@ProviderFor(loginRepository)
+final loginRepositoryProvider =
+    AutoDisposeProvider<LoginRepositoryImp>.internal(
+  loginRepository,
+  name: r'loginRepositoryProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$loginRemoteDataSourceHash,
+      : _$loginRepositoryHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef LoginRemoteDataSourceRef
-    = AutoDisposeProviderRef<LoginRemoteDataSource>;
+typedef LoginRepositoryRef = AutoDisposeProviderRef<LoginRepositoryImp>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- lib/feature/login/domain/repositories/loginn_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-import 'package:authentication_app/feature/login/domain/entities/login_entity.dart';
-
-abstract class LoginRepository {
-  FutureOr<LoginEntity?> getUserLogin({
-    required String email,
-    required String password,
-  });
-}
