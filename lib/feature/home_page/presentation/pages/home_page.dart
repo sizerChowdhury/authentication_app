28f148b9e630132134e45342903b77156d2f3a54
sizer <shahin.reza@brainstation-23.com>
2024-06-12 14:18:26 +0600

    refactor: bring all updated code here

--- lib/feature/home_page/presentation/pages/home_page.dart
+++ lib/feature/home_page/presentation/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:authentication_app/core/gen/assets.gen.dart';
 import 'package:authentication_app/core/navigation/routes/routes_name.dart';
 import 'package:authentication_app/core/widgets/title_underline.dart';
@@ -39,10 +40,26 @@ class _HomePageState extends ConsumerState<HomePage> {
     });
 
     ref.listen(logoutControllerProvider, (_, next) {
-      if (next.value ?? false) {
+      if (next.value?.$1 != null && next.value?.$2 == null) {
         context.push(Routes.login);
-      } else if (next.hasError && !next.isLoading) {
-        print('logout failed');
+      } else if (next.value?.$1 == null && next.value?.$2 != null) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Request Failed!'),
+              content: Text('${next.value?.$2}'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
       }
     });
 
@@ -51,7 +68,7 @@ class _HomePageState extends ConsumerState<HomePage> {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            context.go('/');
+            exit(0);
           },
         ),
         title: Stack(
